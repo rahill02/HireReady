f17b3d72e1fcac02f6bf57fd3a75c8abb55359fd
rahill02 <rahilzaki2005@gmail.com>
2026-06-28 00:06:37 +0530

    Configure frontend for production API URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,9 +21,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "http://localhost:5174",
-                   ],  # React dev server
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://hireready.vercel.app",  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
